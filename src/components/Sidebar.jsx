88d7784d88eb46a32a8b8cfc26b9bd4e67620dde
mannatgupta146 <mannatgupta146@gmail.com>
2025-12-27 19:17:39 +0530

    sidebar also done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,36 +5,36 @@ const Sidebar = () => {
     <div className='bg-[#272135] text-white/70 sidebar w-[15%] h-[97vh] p-3 border border-[#453f55] rounded-2xl flex flex-col gap-2'>
       <div className='flex justify-between'>
         <div className="s1left">
-            <h1>Drameeo</h1>
+            <h1 className='text-xl text-white/90'>Drameeo</h1>
         </div>
         <div className="s1right">
-            mwnu
+            <i className="ri-menu-unfold-3-line text-xl"></i>
         </div>
       </div>
 
-      <div className='w-full h-full flex flex-col justify-between'>
+      <div className='w-full h-full flex flex-col justify-between mt-2'>
         <div className='flex flex-col gap-5'>
-            <div>
-                <h3>Home</h3>
-                <h3>Explore</h3>
-                <h3>Genres</h3>
-                <h3>Favourites</h3>
+            <div className='flex flex-col gap-3 border-b border-gray-600 pb-7'>
+                <h3><i class="ri-home-4-line"></i> Home</h3>
+                <h3><i class="ri-compass-3-line"></i> Explore</h3>
+                <h3><i class="ri-stack-line"></i> Genres</h3>
+                <h3><i class="ri-bookmark-line"></i> Favourites</h3>
+            </div>
+
+            <div className='flex flex-col gap-3 border-b border-gray-600 pb-7'>
+                <h3><i class="ri-play-large-line"></i> Continue Watching</h3>
+                <h3><i class="ri-time-line"></i> Recently Added</h3>
+                <h3><i class="ri-folders-line"></i> My Collections</h3>
+                <h3><i class="ri-download-line"></i> Downloads</h3>
             </div>
 
             <div>
-                <h3>Continue Watching</h3>
-                <h3>Recently Added</h3>
-                <h3>My Collections</h3>
-                <h3>Downloads</h3>
-            </div>
-
-            <div>
-                <h2>Settings</h2>
+                <h2><i class="ri-settings-3-line"></i> Settings</h2>
             </div>
 
         </div>
         <div>
-            <h2>Logout</h2>
+            <h2><i class="ri-logout-circle-r-line"></i>Logout</h2>
         </div>
       </div>
     </div>
